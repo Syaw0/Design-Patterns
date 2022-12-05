3df48447b83bac8ad885af6cc0ad8fccad153d10
Siavash <siamhb7@protonmail.com>
2022-12-05 20:44:26 +0330

    [Prototype(clone)] design pattern

---
+++ DesignPatterns/prototype/prototype.ts
@@ -0,0 +1,42 @@
+
+
+abstract class Shape {
+  x:number
+  y:number
+  color:string
+
+  constructor(source:Shape){
+    this.x =source.x
+    this.y = source.y
+    this.color = source.color
+  }
+
+  abstract clone():Shape
+}
+
+class Rectangle extends Shape{
+  width:number
+  height:number
+  constructor(source:Rectangle) {
+    super(source)
+    this.width = source.width
+    this.height = source.height
+  }
+
+  clone(): Rectangle{
+    return new Rectangle(this)
+  }
+}
+
+
+class Circle extends Shape{
+  radius:number
+  constructor(source:Circle) {
+    super(source)
+    this.radius = source.radius
+  }
+
+  clone(): Circle {
+    return new Circle(this)
+  }
+}
