b50b6d3dd9e1ead2f7baa413957ab61bc3076266
Siavash <siamhb7@protonmail.com>
2022-12-08 17:03:37 +0330

    [Bridge] some explaination

--- DesignPatterns/bridge/bridge.ts
+++ DesignPatterns/bridge/bridge.ts
@@ -1,3 +1,25 @@
+// class Circle{
+//   // do stuff
+// }
+
+// class Triangle{
+//   // do ...
+// }
+
+// class BlackTriangle{
+//   // do ...
+// }
+
+// class RedCircle{
+
+// }
+
+// ! as you can see we are implement two dimension combining together
+// ! this pattern will make you to create a lot of classes and make a code so dirty
+// ? to handle this we need separate two independent dimension to an abstraction and implementation
+// * we just create an MyShape and ColorFull class and just you can see the difference
+// * we don not need to create a huge subclasses and classes just instance :)
+
 class MyShape {
   constructor(public shape: string, public color?: ColorFull) {}
 
